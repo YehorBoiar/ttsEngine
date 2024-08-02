2fc2fd23d81b0e565083923ecf8d527e20127a97
YehorBoiar <boyaregor1@gmail.com>
2024-08-02 11:40:44 +0100

    added synthesize_polly function

--- const.py
+++ const.py
@@ -1,2 +1,8 @@
+import os
+
+
 MAX_TEXT_LENGTH = 150  
 MODEL_NAME = 'tts_models/en/ljspeech/vits'
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_REGION = os.getenv('AWS_REGION')
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from models import TextToSpeechRequest
 import coqui_aiModels.vits as vits
 from fastapi.middleware.cors import CORSMiddleware
 from const import MODEL_NAME
+import boto3
+from botocore.exceptions import BotoCoreError, ClientError
 
 
 app = FastAPI()
@@ -23,6 +25,31 @@ async def synthesize_speech(request: TextToSpeechRequest):
         return {"audio": audio_hex}
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+@app.post("/synthesize_polly")
+def synthesize_speech(aws_access_key_id: str, aws_secret_access_key: str, region_name: str, request: TextToSpeechRequest):
+    try:
+        polly_client = boto3.client(
+            'polly',
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
+            region_name=region_name
+        )
+        response = polly_client.synthesize_speech(
+            Text=request.text,
+            OutputFormat='mp3',
+            VoiceId=request.voice_id
+        )
+
+        if "AudioStream" in response:
+            audio_stream = response["AudioStream"].read()
+            return {"audio": audio_stream.hex()}
+        else:
+            raise HTTPException(status_code=500, detail="Error in synthesizing speech")
+
+    except (BotoCoreError, ClientError) as error:
+        raise HTTPException(status_code=500, detail=str(error))
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8001)
